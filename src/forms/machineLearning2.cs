e0164860e48c73914d7e063b4e596dc6fe758ac2
Raesangur <pascalpinguin@gmail.com>
2019-06-16 22:16:20 -0400

    initial commit

---
+++ src/forms/machineLearning2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace machineLearning2
+{
+    public partial class machineLearning2 : Form
+    {
+        public machineLearning2()
+        {
+            InitializeComponent();
+        }
+
+
+        #region Logging
+
+        public void WriteLine(string text, LogLevel_t level)
+        {
+            /* Empty for now */
+        }
+
+        public void WriteLine(string text)
+        {
+            /* Empty for now */
+        }
+
+        #endregion
+    }
+}
---
+++ src/neuralNet/Neuron.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace machineLearning2
+{
+    class Neuron
+    {
+        private machineLearning2 father;
+        /* Public variables */
+        public string Name;
+        public int Column;
+        public int Line;
+        public double Bias = 0.0;
+        public double Output = 0.0;
+        public double Input = 0.0;
+
+        #region Constructors
+        public Neuron(machineLearning2 parent, string name)
+        {
+            father = parent;
+            Name = name;
+
+            father.WriteLine("Neuron " + name + " created! | " + Bias.ToString(), LogLevel_t.info);
+        }
+
+        public Neuron(machineLearning2 parent, string name, int column, int line)
+        {
+            father = parent;
+            Name = name;
+            Column = column;
+            Line = line;
+
+            father.WriteLine("Neuron " + name + " created! | " + Bias.ToString(), LogLevel_t.info);
+        }
+
+        public Neuron(machineLearning2 parent, string name, double bias)
+        {
+            father = parent;
+            Name = name;
+            Bias = bias;
+
+            father.WriteLine("Neuron " + name + " created! | " + Bias.ToString(), LogLevel_t.info);
+        }
+
+        public Neuron(machineLearning2 parent, string name, double bias, double input)
+        {
+            father = parent;
+            Name = name;
+            Bias = bias;
+            Input = input;
+
+            father.WriteLine("Neuron " + name + " created! | " + Bias.ToString(), LogLevel_t.info);
+        }
+
+        public Neuron(machineLearning2 parent, string name, double bias, int column, int line)
+        {
+            father = parent;
+            Name = name;
+            Bias = bias;
+            Column = column;
+            Line = line;
+
+            father.WriteLine("Neuron " + name + " created! | " + Bias.ToString(), LogLevel_t.info);
+        }
+
+        public Neuron(machineLearning2 parent, string name, double bias, double input, int column, int line)
+        {
+            father = parent;
+            Name = name;
+            Bias = bias;
+            Input = input;
+            Column = column;
+            Line = line;
+
+            father.WriteLine("Neuron " + name + " created! | " + Bias.ToString(), LogLevel_t.info);
+        }
+        #endregion
+
+
+    }
+}
